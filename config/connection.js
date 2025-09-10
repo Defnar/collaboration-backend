38d8e6b33760c2769daf85dfe239a00eba7db5b0
Dennis <dennis-farris@hotmail.com>
2025-09-10 13:31:18 -0400

    created connection to database, and started user controller

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(console.log("database successfully connected"))
+.catch(err => console.log("database failed to connect", err))
+
+const db = mongoose.connection;
+
+module.exports = db;
