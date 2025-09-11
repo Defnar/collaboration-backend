379e2ddde5d41de5f51164dbe4e4a54c21d59bb3
Chandana <kuluri.manichandana@gmail.com>
2025-09-11 09:52:23 -0400

    commit

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
+const db = require("./config/connection");
+const e = require("express");
 const apiRoutes = require("./routes/index");
-
 const app = express();
 const port = process.env.PORT || 8000;
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use("/api", apiRoutes);
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(port, () =>
-      console.log(`Server running at http://localhost:${port}`)
-    );
-  })
-  .catch((err) => console.error(err));
+db.once("open", () => {
+  app.listen(port, `we made this @ http://localhost:${port}`);
+});
